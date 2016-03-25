28ee740ef58b7c94fa4662702697f37208100490
AndroidRul3z <ferrettidario@gmail.com>
2016-03-25 19:29:16 +0100

    script.Print: add missing dots

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -677,13 +677,13 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     # image.  This has the effect of writing new data from the package
     # to the entire partition, but lets us reuse the updater code that
     # writes incrementals to do it.
-    script.Print("Updating system partition")
+    script.Print("Updating system partition...")
     system_tgt = GetImage("system", OPTIONS.input_tmp, OPTIONS.info_dict)
     system_tgt.ResetFileMap()
     system_diff = common.BlockDifference("system", system_tgt, src=None)
     system_diff.WriteScript(script, output_zip)
   else:
-    script.Print("Formatting system partition")
+    script.Print("Formatting system partition...")
     script.FormatPartition("/system")
     script.Mount("/system", recovery_mount_options)
     if not has_recovery_patch:
@@ -741,7 +741,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
       script.Unmount("/system")
 
   script.ShowProgress(0.05, 5)
-  script.Print("Updating kernel")
+  script.Print("Updating kernel...")
   script.WriteRawImage("/boot", "boot.img")
 
   script.ShowProgress(0.2, 10)
